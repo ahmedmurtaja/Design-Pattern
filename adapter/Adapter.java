94f98a976ee553fecb41d75c5eef135ee51da77b
Ahmed Izz Murtaja <56529633+ahmedmurtaja@users.noreply.github.com>
2023-01-07 00:55:48 +0200

    Add Client & Update README

--- adapter/Adapter.java
+++ adapter/Adapter.java
@@ -2,16 +2,19 @@ package adapter;
 
 public class Adapter implements ITargetInterface {
     private Adaptee adaptee;
+
     public Adapter(Adaptee adaptee) {
         this.adaptee = adaptee;
     }
+
     @Override
     public void method1() {
         adaptee.Specific_method1();
     }
+
     @Override
     public void method2() {
         adaptee.Specific_method2();
     }
-    
+
 }
--- adapter/Client.java
+++ adapter/Client.java
@@ -2,6 +2,10 @@ package adapter;
 
 public class Client {
     public static void main(String[] args) {
+        Adaptee adaptee = new Adaptee();
+        ITargetInterface adapter = new Adapter(adaptee);
+        adapter.method1();
+        adapter.method2();
         
     }
     
